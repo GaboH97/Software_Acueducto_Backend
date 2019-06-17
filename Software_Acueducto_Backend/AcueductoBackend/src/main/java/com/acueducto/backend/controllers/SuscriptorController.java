90669cbbc23e642ff551a19922cfb6206ea4bb1e
GaboH97 <gabriel970826@gmail.com>
2019-06-17 17:09:24 -0500

    .

--- Software_Acueducto_Backend/AcueductoBackend/src/main/java/com/acueducto/backend/controllers/SuscriptorController.java
+++ Software_Acueducto_Backend/AcueductoBackend/src/main/java/com/acueducto/backend/controllers/SuscriptorController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -48,8 +49,16 @@ public class SuscriptorController {
 	}
 
 	@DeleteMapping("/suscriptores/{cedula}")
-	public void deleteSuscriptor(@PathVariable String cedula) {
-		suscriptorService.delete(cedula);
+	public @ResponseBody Suscriptor deleteSuscriptor(@PathVariable String cedula) {
+		Suscriptor suscriptor = suscriptorService.findByCedula(cedula);
+		if(suscriptor!=null) {
+			suscriptorService.delete(cedula);
+			return suscriptor;
+		}else {
+			System.out.println("yaper");
+			return null;
+		}
+		
 	}
 
 	@PostMapping("/suscriptores")
@@ -66,6 +75,24 @@ public class SuscriptorController {
 				return "Suscriptor Agregado";
 			} else {
 				return "Ya existe un cliente con cédula "+suscriptor.getCedula();
+			}
+		}
+	}
+	
+	@PutMapping("/suscriptores")
+	@ResponseBody
+	public String updateSuscriptor(@Valid @RequestBody Suscriptor suscriptor, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			StringBuilder builder = new StringBuilder();
+			result.getAllErrors().forEach(e-> builder.append(e.getDefaultMessage().concat(System.getProperty("line.separator"))));
+			return builder.toString();
+		}else { 
+			if (suscriptorService.findByCedula(suscriptor.getCedula()) != null) {
+				suscriptorService.save(suscriptor);
+				return "Suscriptor Editado";
+			} else {
+				return "No existe un cliente con cédula "+suscriptor.getCedula();
 			}
 		}
 	}
