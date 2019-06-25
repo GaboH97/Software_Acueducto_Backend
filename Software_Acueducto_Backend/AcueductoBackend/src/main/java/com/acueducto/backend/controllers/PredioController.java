90f64ba02b47d1b83888f054a639900c12869692
GaboH97 <gabriel970826@gmail.com>
2019-06-25 11:50:48 -0500

    editar predio

--- Software_Acueducto_Backend/AcueductoBackend/src/main/java/com/acueducto/backend/controllers/PredioController.java
+++ Software_Acueducto_Backend/AcueductoBackend/src/main/java/com/acueducto/backend/controllers/PredioController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -71,5 +72,15 @@ public class PredioController {
 		predio.getHistorialPredio().add(historialPredio);
 		predioService.save(predio);
 		return new ResponseEntity<Predio>(predio, HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/predios/{numeroMatricula}")
+	public @ResponseBody Predio updatePredio(@Valid @RequestBody Predio predio, @PathVariable String numeroMatricula){
+		if(predioService.findByNumeroMatriculaWithSuscriptor(numeroMatricula)!=null) {
+			predioService.save(predio);
+			return predio;
+		}else {
+			return null;
+		}
 	}
 }
